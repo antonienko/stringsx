9f640a66cfc6c7e052bb8a5d4ca5b51a02059060
AntonioHS <antonienko@gmail.com>
2019-02-18 18:03:56 +0100

    Initial commit

---
+++ substring.go
@@ -0,0 +1,23 @@
+package stringsx
+
+// Get the substring of a string, taking care of not breaking unicode characters
+// For the optional arguments, first is the offset, second one is the length
+func Substring(str string, limits ...int) string {
+	var offset, length int
+	var runes []rune
+	switch {
+	case len(limits) == 0:
+		return str
+	case len(limits) > 2:
+		panic("the limits parameter should consist of 2 integers: offset and length")
+	case len(limits) == 2:
+		runes = []rune(str)
+		offset = limits[0]
+		length = limits[1]
+	case len(limits) == 1:
+		runes = []rune(str)
+		offset = limits[0]
+		length = len(runes)
+	}
+	return string(runes[offset:length])
+}
---
+++ substring_test.go
@@ -0,0 +1,114 @@
+package stringsx
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSubstring_NoLimits(t *testing.T) {
+	testCases := map[string]struct {
+		str            string
+		expectedSubstr string
+	}{
+		"no limits, no unicode, should return whole string": {
+			"How you doin'",
+			"How you doin'",
+		},
+		"no limits, unicode, should return whole string": {
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+		},
+	}
+	for title, tc := range testCases {
+		t.Run(title, func(t *testing.T) {
+			assert.Equal(t, tc.expectedSubstr, Substring(tc.str))
+		})
+	}
+}
+
+func TestSubstring_OnlyOffset(t *testing.T) {
+	testCases := map[string]struct {
+		str            string
+		offset         int
+		expectedSubstr string
+	}{
+		"no length, no unicode, offset 1 should remove 1st char": {
+			"How you doin'",
+			1,
+			"ow you doin'",
+		},
+		"no length, unicode, offset 1 should remove 1st char": {
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+			1,
+			"èàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+		},
+	}
+	for title, tc := range testCases {
+		t.Run(title, func(t *testing.T) {
+			assert.Equal(t, tc.expectedSubstr, Substring(tc.str, tc.offset))
+		})
+	}
+}
+
+func TestSubstring_OnlyBothLimits(t *testing.T) {
+	testCases := map[string]struct {
+		str            string
+		offset         int
+		length         int
+		expectedSubstr string
+	}{
+		"both limits, no unicode, should return whole string": {
+			"How you doin'",
+			0,
+			13,
+			"How you doin'",
+		},
+		"both limits, unicode, should return whole string": {
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+			0,
+			21,
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+		},
+		"both limits, no unicode, should remove first char": {
+			"How you doin'",
+			1,
+			13,
+			"ow you doin'",
+		},
+		"both limits, unicode, should remove first char": {
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+			1,
+			21,
+			"èàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+		},
+		"both limits, no unicode, should remove last char": {
+			"How you doin'",
+			0,
+			12,
+			"How you doin",
+		},
+		"both limits, unicode, should remove last char": {
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+			0,
+			20,
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º",
+		},
+		"both limits, no unicode, should remove first 3 and last 4 chars": {
+			"How you doin'",
+			3,
+			9,
+			" you d",
+		},
+		"both limits, unicode, should remove first 3 and last 4 chars": {
+			"ùèàùêÊäÄöÖüÜçÇ¿ß¡£§º©",
+			3,
+			17,
+			"ùêÊäÄöÖüÜçÇ¿ß¡",
+		},
+	}
+	for title, tc := range testCases {
+		t.Run(title, func(t *testing.T) {
+			assert.Equal(t, tc.expectedSubstr, Substring(tc.str, tc.offset, tc.length))
+		})
+	}
+}
